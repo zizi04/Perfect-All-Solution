cf73778cc04c7a2668863a1844ea5306d7ae4f03
zizi04 <disguy3939@naver.com>
2016-10-13 00:35:58 +0900

    ch07

---
+++ ch07/7-15/goto.c
@@ -0,0 +1,17 @@
+// file: goto.c
+
+#include <stdio.h> 
+
+int main(void)
+{
+	int count = 1;
+
+loop:
+	printf("%3d", count);
+	if (++count <= 10) 
+		goto loop;
+
+	printf("\n프로그램을 종료합니다.\n");
+
+	return 0;
+}
